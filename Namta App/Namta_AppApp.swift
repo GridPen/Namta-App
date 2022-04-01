ddf3d64eba04fed705010ae8cd104ac08bac3449
Debashish M <10178321+GridPen@users.noreply.github.com>
2022-04-01 21:09:17 +0600

    Initial Commit

---
+++ Namta App/Namta_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Namta_AppApp.swift
+//  Namta App
+//
+//  Created by Debashish Mondal on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Namta_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
