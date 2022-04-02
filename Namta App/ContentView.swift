2f41466b8d71d3acd173ac8cb711ed5fd6cf1abc
Debashish M <10178321+GridPen@users.noreply.github.com>
2022-04-02 19:54:29 +0600

    gradient

--- Namta App/ContentView.swift
+++ Namta App/ContentView.swift
@@ -15,48 +15,52 @@ struct ContentView: View {
     @State private var showingAlert = false
     @State private var tables = 2
     @State private var questionAmount = 5
-    var qNumber = [5,10,20]
+    @State private var qNumber = [5,10,20]
     @State private var nextQuestion = 0
     @State private var tapCount = 1
     
     
     var body: some View {
-        VStack(spacing:20){
-            VStack{
-                Stepper("Select multiplication table: \(tables)", value: $tables, in: 1...20)
-            }
-            VStack{
-                Picker("Seclect question amount", selection: $questionAmount) {
-                    ForEach(qNumber, id: \.self) { qNumber in
-                        Text("\(qNumber)")
+        ZStack {
+            LinearGradient(colors: [.purple.opacity(0.3),.blue.opacity(0.4)], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            VStack(spacing:20){
+                VStack{
+                    Stepper("Select multiplication table: \(tables)", value: $tables, in: 1...20)
+                }
+                VStack{
+                    Picker("Seclect question amount", selection: $questionAmount) {
+                        ForEach(qNumber, id: \.self) { qNumber in
+                            Text("\(qNumber)")
+                        }
+                    }
+                }.pickerStyle(.segmented)
+                
+                Text("Wrire the correct Answer?")
+                    .font(.headline)
+                
+                VStack{
+                    Text("Question Number \(tapCount) of \(questionAmount): \(tables) X \(number1) = ")
+                    HStack{
+                        TextField("write your answer here", text: $userAnswer)
                     }
                 }
-            }.pickerStyle(.segmented)
-            
-            Text("Wrire the correct Answer?")
-                .font(.headline)
-            
-            VStack{
-                Text("Question Number \(tapCount) of \(questionAmount): \(tables) X \(number1) = ")
-                HStack{
-                    TextField("write your answer here", text: $userAnswer)
+                
+                Button("Check") {
+                    showingAlert = true
+                    tapCount += 1
+                }
+                .alert("Question \(questionHave())", isPresented: $showingAlert) {
+                    Button("OK", role: .cancel) {  }
                 }
             }
             
-            Button("Next"){
-                showingAlert = true
-                tapCount += 1
-            }
-            .alert("Question \(asn())", isPresented: $showingAlert) {
-                Button("OK", role: .cancel) {  }
-            }
+            
             
         }
-        
-
-        
     }
-    func asn() -> Int {
+    
+    func questionHave() -> Int {
         var a = nextQuestion + questionAmount
         var b = a - 1
         return b
@@ -70,3 +74,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
