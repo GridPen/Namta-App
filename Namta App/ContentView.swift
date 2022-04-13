dc8acc6b62a5772c56ae1b836a5c4ff1cd203521
Debashish M <10178321+GridPen@users.noreply.github.com>
2022-04-13 18:34:57 +0600

    fresh

--- Namta App/ContentView.swift
+++ Namta App/ContentView.swift
@@ -10,68 +10,70 @@ import SwiftUI
 struct ContentView: View {
     @State private var correctAns = 0
     @State var number1 = 2
-    @State var number2 = Int.random(in: 0...10)
     @State private var userAnswer = ""
     @State private var showingAlert = false
     @State private var tables = 2
-    @State private var questionAmount = 5
+    @State private var questionAmount = 0
     @State private var qNumber = [5,10,20]
     @State private var nextQuestion = 0
-    @State private var tapCount = 1
+    @State private var tapCount = 0
     
     
     var body: some View {
-        ZStack {
-            LinearGradient(colors: [.purple.opacity(0.3),.blue.opacity(0.4)], startPoint: .topLeading, endPoint: .bottomTrailing)
-                .ignoresSafeArea()
-            VStack(spacing:20){
-                VStack{
-                    Stepper("Select multiplication table: \(tables)", value: $tables, in: 1...20)
-                }
-                VStack{
-                    Picker("Seclect question amount", selection: $questionAmount) {
-                        ForEach(qNumber, id: \.self) { qNumber in
-                            Text("\(qNumber)")
+        NavigationView{
+            ZStack {
+                LinearGradient(colors: [.purple.opacity(0.3),.blue.opacity(0.4)], startPoint: .topLeading, endPoint: .bottomTrailing)
+                    .ignoresSafeArea()
+                VStack(spacing:20){
+                    VStack{
+                        Section{
+                            Stepper("Select multiplication table: \(tables)", value: $tables, in: 1...20)
                         }
                     }
-                }.pickerStyle(.segmented)
-                
-                Text("Wrire the correct Answer?")
-                    .font(.headline)
-                    .foregroundColor(.orange)
-                
-                VStack{
-                    Text("Question Number \(tapCount) of \(questionAmount): \(tables) X \(number1) = ")
-                    HStack{
-                        TextField("write your answer here",text: $userAnswer)
+                    VStack{
+                        Section{
+                            Text("Seclect question amount:")
+                                .font(.headline)
+                            Picker("Seclect question amount", selection: $questionAmount) {
+                                ForEach(qNumber, id: \.self) { qNumber in
+                                    Text("\(qNumber)")
+                                }
+                            }
+                        }
+                    }.pickerStyle(.segmented)
+                    
+                    Text("Wrire the correct Answer?")
+                        .font(.headline)
+                        .foregroundColor(.primary)
+                    
+                    VStack{
+                        Text("Question Number \(tapCount) of \(questionAmount): \(tables) X \(number1) = ")
+                        HStack{
+                            TextField("write your answer here",text: $userAnswer)
+                        }
+                    }
+                    
+                    Button("Check") {
+                        let generateRandom = Int.random(in: 2...10)
+                        number1 = generateRandom
+                        
+                        showingAlert = true
+                        tapCount += 1
+                        if tapCount == questionAmount{
+                            print ("Hello")
+                        } else {
+                            print("Billo")
+                        }
+                    }
+                    
+                    .alert("Question \(tapCount)", isPresented: $showingAlert) {
+                        Button("OK", role: .cancel) {  }
                     }
                 }
-                
-                Button("Check") {
-                    let playerRandom = Int.random(in: 2...10)
-                    number1 = playerRandom
-                    showingAlert = true
-                    tapCount += 1
-                }
-                
-                .alert("Question \(questionHave())", isPresented: $showingAlert) {
-                    Button("OK", role: .cancel) {  }
-                }
             }
-            
-            
-            
         }
     }
-    
-    func questionHave() -> Int {
-        var a = nextQuestion + questionAmount
-        var b = a - 1
-        return b
-    }
 }
-
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
